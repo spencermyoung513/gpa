32efcd97569bdcdb661ca8d0da2847e12cbe68f6
Spencer Young <spencermyoung513@gmail.com>
2025-06-05 20:48:20 -0600

    Added option for dropout, batch norm in link predictor. Also fixed predictions viewer

--- src/gpa/models/link_predictors.py
+++ src/gpa/models/link_predictors.py
@@ -33,12 +33,16 @@ class MLPLinkPredictor(LinkPredictor):
     def __init__(
         self,
         layer_widths: list[int],
+        use_batch_norm: bool = False,
+        dropout: float | None = None,
         strategy: Literal["concat", "hadamard", "add"] = "hadamard",
     ):
         """Initialize an `MLPLinkPredictor`.
 
         Args:
             layer_widths (list[int]): The desired widths of the linear layers in the MLP.
+            use_batch_norm (bool): Whether to use batch normalization between linear layers. Defaults to False.
+            dropout (float): The dropout rate to use after each linear layer. Defaults to 0.0.
             strategy (Literal["concat", "hadamard", "add"]): The strategy used to combine the node embeddings.
         """
         super().__init__()
@@ -48,7 +52,11 @@ class MLPLinkPredictor(LinkPredictor):
         for i, width in enumerate(layer_widths):
             self.layers.append(nn.LazyLinear(out_features=width))
             if i < len(layer_widths) - 1:
+                if use_batch_norm:
+                    self.layers.append(nn.BatchNorm1d(width))
                 self.layers.append(nn.ReLU())
+                if dropout is not None:
+                    self.layers.append(nn.Dropout(dropout))
         self.link_predictor = nn.LazyLinear(out_features=1)
 
     def forward(
